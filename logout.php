26ea5c094ce00d9e87de12d56db6c877d0326d9e
webfans <cc_lovesky@163.com>
2016-12-10 10:39:42 +0800

    add newfile named *logout.php* used to clear user Cookie

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+//指定一个常量 用来授权能不能调用文件
+define('IN_TG',true);
+//定义一个常量 用来指定本页的内容
+define('SCRIPT','login');
+//引入公共文件
+require dirname(__FILE__).'/includes/common.inc.php';
+//退出清空cookie
+cookie_d();
+
