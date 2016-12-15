994905f3329ffdbe6b8aa30b361e1197305f5043
webfans <cc_lovesky@163.com>
2016-12-15 15:11:37 +0800

    modify footer copyright

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -8,6 +8,6 @@ $end_time=runtime();
 <div id="footer">
     <p>本程序执行耗时为:<?php echo $end_time-START_TIME;?></p>
     <p>版权所有，盗版必究</p>
-    <p>本程序由<span>我爱你心心理咨询论坛</span>提供&copy;www.psy520.cn</p>
+    <p>本程序由<span>心理咨询论坛</span>提供<span></span>Author:CAOCONG</span>&copy;xlzx.online</p>
 </div>
 
