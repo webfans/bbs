7655309c4596ac09ffdb8b3194fcb8458632a4dc
webfans <cc_lovesky@163.com>
2016-12-16 09:49:57 +0800

    ADD member.inc..php file to includes

---
+++ includes/member.inc.php
@@ -0,0 +1,19 @@
+<?php
+//防止恶意调用
+if (!defined('IN_TG')){
+    exit('非法调用');
+}
+?>
+<h2>会员中心</h2>
+<dl>
+    <dt>账户管理</dt>
+    <dd><a href="###">人个信息</a> </dd>
+    <dd><a href="###">修改资料</a> </dd>
+</dl>
+<dl>
+    <dt>其它管理</dt>
+    <dd><a href="###">短信查询</a> </dd>
+    <dd><a href="###">好友设置</a> </dd>
+    <dd><a href="###">查询花朵</a> </dd>
+    <dd><a href="###">人个相册</a> </dd>
+</dl>
