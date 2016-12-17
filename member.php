a1d5a2767fd08ff8aaf70e4955d49186bbc91d13
webfans <cc_lovesky@163.com>
2016-12-17 11:53:17 +0800

    use htmlspechialchars() to escape html chars from mysql.bbs_user

--- member.php
+++ member.php
@@ -8,6 +8,40 @@ define('IN_TG',true);
 define('SCRIPT','member');
 //引入公共文件
 require dirname(__FILE__).'/includes/common.inc.php';
+if (isset($_COOKIE['username'])){
+    //获取数据集
+    $sql="select u_username,u_sex,u_face,u_email,u_url,u_qq,u_regtime,u_level from bbs_user where u_username='{$_COOKIE['username']}'";;
+    $rows=fetch_array($sql);
+    //如果有数据，,或者数据库没有此用户，伪造用户cookie
+    if ($rows){
+       $html=array();
+       $html['username']=$rows['u_username'];
+       $html['sex']=$rows['u_sex'];
+       $html['face']=$rows['u_face'];
+       //$html['email']=$rows['u_email']."<html>";//转义测试
+       $html['email']=$rows['u_email'];
+       $html['url']=$rows['u_url'];
+       $html['qq']=$rows['u_url'];
+       $html['regtime']=$rows['u_regtime'];
+       //一次性转义数组，也可以分别每一行
+       $html=html_spec($html);
+       switch ($rows['u_level']){
+           case 0:
+               $html['level']='普通会员';
+               break;
+           case 1:
+               $html['level']='管理员';
+               break;
+           default:
+               $html['level']='Guest';
+       }
+    }else{
+        alert_back('此用户不存在');
+    }
+}else{
+    alert_back('非法登录');
+}
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -28,14 +62,14 @@ require ROOT_PATH.'includes/header.inc.php';
     <div id="member_main">
         <h2>会员管理中心</h2>
         <dl>
-            <dd>用 户 名:</dd>
-            <dd>性&emsp;&emsp;别：</dd>
-            <dd>头&emsp;&emsp;像：</dd>
-            <dd>电子邮件：</dd>
-            <dd>主&emsp;&emsp;页：</dd>
-            <dd>Q&emsp;&emsp;&emsp;Q:</dd>
-            <dd>注册时间：</dd>
-            <dd>身&emsp;&emsp;份：</dd>
+            <dd>用 户 名:<?php echo $html['username'];?></dd>
+            <dd>性&emsp;&emsp;别：<?php echo $html['sex'];?></dd>
+            <dd>头&emsp;&emsp;像：<?php echo $html['face'];?></dd>
+            <dd>电子邮件：<?php echo $html['email'];?></dd>
+            <dd>主&emsp;&emsp;页：<?php echo $html['url'];?></dd>
+            <dd>Q&emsp;&emsp;&emsp;Q:<?php echo $html['qq'];?></dd>
+            <dd>注册时间：<?php echo $html['regtime'];?></dd>
+            <dd>身&emsp;&emsp;份：<?php echo $html['level'];?></dd>
         </dl>
     </div>
 
