16bf1459416e553c05651769c4697abf9c166c30
webfans <cc_lovesky@163.com>
2016-12-29 23:02:05 +0800

    small patch

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -11,7 +11,7 @@ if (!defined('IN_TG')){
 
         <?php
         if (isset($_COOKIE['username'])){
-            echo '<li><a href="member.php">'.$_COOKIE['username'].'·个人中心</a> </li>';
+            echo '<li><a href="member.php">'.$_COOKIE['username'].'·个人中心</a> '.$msg.'</li>';
             echo "\n";
         }
         else{
