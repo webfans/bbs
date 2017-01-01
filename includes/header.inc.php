fbe8c41618541fb83942812790cd853fff3495fd
webfans <cc_lovesky@163.com>
2017-01-01 16:24:47 +0800

    modify website title
    modify website title

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -5,7 +5,7 @@ if (!defined('IN_TG')){
 }
 ?>
 <div id="header">
-    <h1><a href="../../index.php">psy520多用户留言系统</a></h1>
+    <h1><a href="../../index.php">多用户留言系统</a></h1>
     <ul>
         <li><a href="index.php">首页</a></li>
 
