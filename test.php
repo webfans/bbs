bfb639739de2c9fb76f47cc6bc9a39244da2736a
webfans <cc_lovesky@163.com>
2017-01-01 16:33:04 +0800

    test

--- test.php
+++ test.php
@@ -1,64 +1,26 @@
 <?php
-session_start();
-//指定一个常量 用来授权能不能调用文件
-define('IN_TG',true);
-//定义一个常量 用来指定本页的内容
-define('SCRIPT','register');
-//引入公共文件
-
-require dirname(__FILE__).'/includes/common.inc.php';
-
-//echo sha1_uniqid();
-$d['active']=sha1_uniqid();
-connect();
-selectdb();
-######错误无法插入完整插入u_acitve,u_uniqid#####原因是 数据库 数据类型错误的设为INT 应该为CHAR
-//$sql="insert into bbs_user (u_active) values ('{$d['active']}')";
-echo '<br/>';
-
-print_r($d['active']);
-echo '<br/>';
-query(
-    "insert into bbs_user(
-                                     u_uniqid,
-                                     u_active,
-                                     u_username,
-                                     u_password,
-                                     u_question,
-                                     u_sex,
-                                     u_face,
-                                     u_answer,
-                                     u_email,
-                                     u_qq,
-                                     u_url,
-                                     u_regtime,
-                                     u_lasttime,
-                                     u_lastip
-                                     )                               
-                               VALUES(
-                                      '12345fdgg',
-                                      '{$d['active']}',
-                                      '123456',
-                                      '12345678',
-                                      '1234567',
-                                      '男',
-                                      '123jddd',
-                                      '123456',
-                                      'cclove@123.com',
-                                      '123456789',
-                                      'www.baidu.com',
-                                       NOW(),
-                                       NOW(),
-                                      '{$_SERVER['REMOTE_ADDR']}'
-                                     )"
-) or die('sql执行错误'.mysql_error());
-//query($sql);
-if (affetched_rows()==1){
-    location('数据测试插入成功','login.php');
-}else {
-    location('数据测试插入失败', 'reisger.php');
+$fp=fopen('new.xml','w');
+if (!$fp){
+    exit('系统错误，文件不存在');
 }
-#######################################################################################
-//$update_sql="update bbs_user set u_active=null where u_acitve='$active'";
-
-
+flock($fp,LOCK_EX);//锁定
+    $string="<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n";
+    fwrite($fp,$string,strlen($string));
+    $string="<vip>\r\n";
+    fwrite($fp,$string,strlen($string));
+    $string="\t<id>5</id>\r\n";
+    fwrite($fp,$string,strlen($string));
+    $string="\t<username>face</username>\r\n";
+    fwrite($fp,$string,strlen($string));
+    $string="\t<sex>男</sex>\r\n";
+    fwrite($fp,$string,strlen($string));
+    $string="\t<face>images/face/m01.gif</face>\r\n";
+    fwrite($fp,$string,strlen($string));
+    $string="\t<email>cclovesky@gmail.com</email>\r\n";
+    fwrite($fp,$string,strlen($string));
+    $string="\t<url>xlzx.online</url>\r\n";
+    fwrite($fp,$string,strlen($string));
+    $string="</vip>\r\n";
+    fwrite($fp,$string,strlen($string));
+flock($fp,LOCK_UN);//解锁
+fclose($fp);
