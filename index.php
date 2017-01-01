08d3334d826a05e5117b5ec4cc1e1570aa0b4980
webfans <cc_lovesky@163.com>
2017-01-01 16:29:45 +0800

    add new member display

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //error输出
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 //指定一个常量 用来授权能不能调用文件
 define('IN_TG',true);
 //定义一个常量 用来指定本页的内容
@@ -10,13 +10,17 @@ define('SCRIPT','index');
 //引入公共文件
 require dirname(__FILE__).'/includes/common.inc.php';
 //echo ROOT_PATH;
+//读取用户信息xml
+$_html=html_spec(get_xml('userinfo.xml'));
+//print_r($_html);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    <script type="text/javascript" src="js/blog.js"></script>
     <?php require ROOT_PATH.'includes/title.inc.php'?>
-    <title>PSY520多用户留言系统</title>
+    <title>多用户留言系统</title>
 </head>
 <body>
 <?php
@@ -28,6 +32,16 @@ require ROOT_PATH.'includes/header.inc.php';
 </div>
 <div id="user">
     <h2>新进会员</h2>
+    <dl>
+        <dd class="user"><?php echo $_html['username'];?>(<?php echo $_html['sex'];?>)</dd>
+        <dt><img src="<?php echo $_html['face'];?>" alt="root"/></dt>
+        <dd class="message"><a name="message" title="<?php echo $_html['id']?>">发消息</a></dd>
+        <dd class="friend"><a name="friend" title="<?php echo $_html['id']?>">加好友</a></dd>
+        <dd class="guest">写留言</dd>
+        <dd class="flower"><a name="flower">">给他送花</a></dd>
+        <dd class="email"><a href="mailto:<?php echo $_html['email'];?>">邮件:<?php echo $_html['email'];?></a></dd>
+        <dd class="url"><a href="<?php echo $_html['url'];?>" target="_blank">网址:<?php echo $_html['url'];?></a></dd>
+    </dl>
 </div>
 <div id="pics">
     <h2>最新图片</h2>
