0ccd11cbbe7b854bed1d3fe1226e50bb393d8f22
webfans <cc_lovesky@163.com>
2017-01-07 10:21:33 +0800

    used to  select image with post article

---
+++ js/qopener.js
@@ -0,0 +1,16 @@
+/**
+ * Created by bigface on 2016/11/25.
+ */
+window.onload=function () {
+    var img=document.getElementsByTagName('img');
+    for (var i=0;i<img.length;i++){
+        img[i].onclick=function () {
+            _opener(this.alt);
+        };
+    }
+
+};
+//改变父窗口texearea的值
+function _opener(son_src){
+    opener.document.getElementsByTagName('form')[0].content.value+='[img]'+son_src+'[/img]'
+}
