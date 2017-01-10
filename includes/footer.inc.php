e2a236161e1c3dc68eb231bcd881d2004aa09f96
webfans <cc_lovesky@163.com>
2017-01-10 22:20:39 +0800

    modify footer bar

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -7,7 +7,7 @@ $end_time=runtime();
 ?>
 <div id="footer">
     <p>本程序执行耗时为:<?php echo $end_time-START_TIME;?></p>
-    <p>版权所有，盗版必究</p>
-    <p>本程序由<span>心理咨询论坛</span>提供<span></span>Author:CAOCONG</span>&copy;xlzx.online</p>
+    <p>本程序由<span>心理咨询论坛提供</span><strong>&nbsp;作者:CAOCONG</strong></p>
+    <p>Copyright © 2016-2017 xlzx.online All Rights Reserved. 备案号：豫ICP备160399351-1</p>
 </div>
 
