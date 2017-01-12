aee983efb6cebe95cd1d8c4a09840acf810ad20c
webfans <cc_lovesky@163.com>
2017-01-12 16:07:39 +0800

    加入个性签名功能

--- article.php
+++ article.php
@@ -9,7 +9,7 @@ define('IN_TG',true);
 define('SCRIPT','article');
 //引入公共文件
 require dirname(__FILE__).'/includes/common.inc.php';
-//处理回贴
+//处理跟贴
 if ($_GET['action']=='RE'){
     //include 在需要时引入
     include ROOT_PATH.'includes/check.func.php';
@@ -19,7 +19,7 @@ if ($_GET['action']=='RE'){
     //2.对比唯一标识符
     if (!!$row=fetch_array("select u_uniqid from bbs_user where u_username='{$_COOKIE['username']}'")){
         safe_uniquid($row['u_niqid'],$_SESSION['uniqid']);
-        //接受回贴表单数据
+        //接受跟贴表单数据
         $clean=array();
         $clean['reid']=$_POST['reid'];
         $clean['retype']=$_POST['retype'];
@@ -85,7 +85,9 @@ if (isset($_GET['id'])){
         query("update bbs_article set art_readcount=art_readcount+1 where art_id='{$_GET['id']}'");
         
         //拿出用户名去查用户信息(email,url)
-        $_rows=fetch_array("select u_id,u_sex,u_face,u_email,u_url from bbs.bbs_user WHERE u_username='{$_html['username_subject']}'");
+        $_rows=fetch_array("select u_id,u_sex,u_face,u_email,u_url,u_switch,u_autograph 
+                            from bbs.bbs_user WHERE u_username='{$_html['username_subject']}'
+                          ");
         if (!!$_rows){
            //读取用户信息
            $_html['userid']=$_rows['u_id'];
@@ -93,6 +95,8 @@ if (isset($_GET['id'])){
            $_html['face']=$_rows['u_face'];
            $_html['email']=$_rows['u_email'];
            $_html['url']=$_rows['u_url'];
+           $_html['switch']=$_rows['u_switch'];
+           $_html['autograph']=$_rows['u_autograph'];
            $_html=html_spec($_html);
            //点击【编辑】 修改主题贴模块
            if ($_html['username_subject']==$_COOKIE['username']) {
@@ -102,8 +106,16 @@ if (isset($_GET['id'])){
             if ($_html['modifydate']!='0000-00-00 00:00:'){
                $_html['modifydate_new']='本贴已由['.$_html['username_subject'].']于'.$_html['modifydate'].'修改过';
             }
+            //回复楼主
+            if(isset($_COOKIE['username'])){
+                $_html['subjectreply_html']='<span><a href="#replyform" name="reply" title="回复1楼的'.$_html['username_subject'].'">[回复]</a></span>';
+            }
+            //个性签名
+            if ($_html['switch']==1){
+                $_html['autograph_html']=' <p class="autograph"><span>个性签名:</span>'.$_html['autograph'].'</p>';
+            }
 
-           //#2.读取回帖信息#
+           //#2.读取跟帖信息#
                      //创建一个全局变量，做一个带参数的分页
         			  global $_id;
         			  $_id='id='.$_html['subject_id'].'&';
@@ -116,7 +128,7 @@ if (isset($_GET['id'])){
                                   WHERE art_reid='{$_html['subject_id']}' 
                                                        ORDER BY art_date ASC 
                                                        LIMIT $_pagenum,$_pagesize";
-                //取得回贴数据集
+                //取得跟贴数据集
                 $reply_result=query($reply_sql);
 
         }else{
@@ -163,12 +175,15 @@ require ROOT_PATH.'includes/header.inc.php';
                 <div class="user">
                     <span><?php echo $_html['subject_modify']?> 1楼</span><?php echo $_html['username_subject']?>| 发表于：<?php echo $_html['date']?>
                 </div>
-                <h3>主题：<?php echo $_html['title']?> <img src="images/icon<?php echo $_html['type']?>.gif" alt="icon"> </h3>
+                <h3>
+                    主题：<?php echo $_html['title']?> <img src="images/icon<?php echo $_html['type']?>.gif" alt="icon">
+                    <?php echo $_html['subjectreply_html']?>
+                </h3>
                 <div class="detail">
                     <?php echo ubb($_html['content'])?>
+                    <?php echo $_html['autograph_html']?>
                 </div>
                 <div class="read">
-
                     阅读量：(<?php echo $_html['readcount']?>) 评论量：(<?php echo $_html['comment']?>)&nbsp;<?php echo $_html['modifydate_new']?>
                 </div>    　
             </div>
@@ -176,7 +191,7 @@ require ROOT_PATH.'includes/header.inc.php';
     <?php }?>
     <p class="line"></p><!--分割线-->
     <?php
-#START回帖楼层开始START#
+#START跟帖楼层开始START#
         ####楼层算法解释START#################################################
         /*
         $floor=2;楼主算1楼，第一个回贴的算2楼
@@ -213,15 +228,18 @@ require ROOT_PATH.'includes/header.inc.php';
             $_html['content']=$rows['art_content'];
             $_html['reply_modifydate']=$rows['art_lastmodify'];
             $_html['date']=$rows['art_date'];
-        //拿出用户名去查用户信息(email,url)
-        $_rows=fetch_array("select u_id,u_sex,u_face,u_email,u_url from bbs.bbs_user WHERE u_username='{$_html['username_reply']}'");
+        //拿出用户名去查跟帖用户信息(email,url)
+        $_rows=fetch_array("select u_id,u_sex,u_face,u_email,u_url,u_switch,u_autograph 
+                             from bbs.bbs_user WHERE u_username='{$_html['username_reply']}'");
         if (!!$_rows){
-            //读取用户信息
+            //读取跟帖用户信息
             $_html['userid']=$_rows['u_id'];
             $_html['sex']=$_rows['u_sex'];
             $_html['face']=$_rows['u_face'];
             $_html['email']=$_rows['u_email'];
             $_html['url']=$_rows['u_url'];
+            $_html['switch']=$_rows['u_switch'];
+            $_html['reply_autograph']=$_rows['u_autograph'];
             $_html=html_spec($_html);
 
             //点击【编辑】 修改回复贴模块
@@ -235,6 +253,7 @@ require ROOT_PATH.'includes/header.inc.php';
             if ($_html['reply_modifydate']!='0000-00-00 00:00:'){
                 $_html['reply_modifydate_new']='本贴已由['.$_html['username_reply'].']于'.$_html['reply_modifydate'].'修改过';
             }
+            //楼层沙发设置
             //bug表现状况，只要是楼主抢了沙发，后边回复的所有贴，左侧用户名都会变成楼主的信息，并且下面的楼层都成沙发了
             /*if ($floor==2){
                 if ($_html['username']==$_html['username_subject']){
@@ -251,19 +270,28 @@ require ROOT_PATH.'includes/header.inc.php';
                     $_html['username_html']=$_html['username_reply'].'(沙发)';
                 }
             }
-            elseif ($floor==3&&$_html['username_reply']!==$_html['username_subject']){
+            //此处还有问题，跟帖强到了沙发，还能再做板凳吗？
+            elseif ($_page==1&&$floor==3&&$_html['username_reply']!==$_html['username_subject']){
                 $_html['username_html']=$_html['username_reply'].'(板凳)';
             } else{
                 $_html['username_html']=$_html['username_reply'];
             }
-
+            //跟帖回复 只有在登录情况下才 显示[回复]
+            if (isset($_COOKIE['username'])){
+                $_html['reply_floor']='回复'.($floor+(($_page-1)*$_pagesize)).'楼的'.$_html['username_reply'].'';
+                $_html['reply_html']='<span><a href="#replyform" name="reply" title="'.$_html['reply_floor'].'">[回复]</a></span>';
+            }
+            //个性签名 //不能写在这里，因为有循环会累积，溢出，直接写在下面的跟贴界面处
+           /* if ($_html['switch']==1){
+                $_html['reply_autograph']=' <p class="autograph"><span>个性签名:</span>'.$_html['autograph'].'</p>';
+            }*/
 
         }else{
             //这个用户可能已经被删除，先不做这块儿
             echo '这个用户可能已经被删除';
         }
     ?>
-        <!--#以下是回贴楼界面-->
+        <!--#以下是跟贴楼界面-->
         <div class="re">
             <dl>
                 <dd class="user"><?php echo $_html['username_html'];?>(<?php echo $_html['sex'];?>)</dd>
@@ -277,11 +305,19 @@ require ROOT_PATH.'includes/header.inc.php';
             </dl>
             <div class="content">
                 <div class="user">
-                    <span><?php echo $_html['reply_modify']?>&nbsp;<?php echo $floor+(($_page-1)*$_pagesize);//楼层算法?>楼</span><?php echo $_html['username']?>| 发表于：<?php echo $_html['date']?>
+                    <span><?php echo $_html['reply_modify']?>&nbsp;<?php echo $floor+(($_page-1)*$_pagesize);//楼层算法?>楼</span><?php echo $_html['username_reply']?>| 发表于：<?php echo $_html['date']?>
                 </div>
-                <h3>主题：<?php echo $_html['retitle']?> <img src="images/icon<?php echo $_html['type']?>.gif" alt="icon"> </h3>
+                <h3>
+                    主题：<?php echo $_html['retitle']?> <img src="images/icon<?php echo $_html['type']?>.gif" alt="icon">
+                    <?php echo $_html['reply_html']?>
+                </h3>
                 <div class="detail">
                     <?php echo ubb($_html['content'])?>
+                    <?php
+                    if ($_html['switch']==1){
+                        echo ' <p class="autograph"><span></span>'.ubb($_html['reply_autograph']).'</p>';
+                    }
+                    ?>
                 </div>
                 <div class="read"><?php echo $_html['reply_modifydate_new']?></div>
             </div>
@@ -294,8 +330,9 @@ require ROOT_PATH.'includes/header.inc.php';
     paging(1);
 #END回贴楼层结束END#
     ?>
-    <!--##以下是[回贴表单],仅登录可见-->
+    <!--##以下是[跟贴表单],仅登录可见-->
     <?php if (isset($_COOKIE['username'])){?>
+        <a name="replyform"></a><!--回复锚点-->
         <form action="?action=RE" method="post">
             <input type="hidden" name="reid" value="<?php echo $_html['subject_id']?>">
             <input type="hidden" name="retype" value="<?php echo $_html['type']?>">
