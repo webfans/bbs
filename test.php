7de9901f1004d629861426ad940205991622c8da
webfans <cc_lovesky@163.com>
2017-01-17 22:12:51 +0800

    test modify

--- test.php
+++ test.php
@@ -1,26 +1,22 @@
 <?php
-$fp=fopen('new.xml','w');
-if (!$fp){
-    exit('系统错误，文件不存在');
+$conn=mysql_connect('localhost','root','398692315');
+if (!$conn) {
+ 	die('Cannot connect the database'.mysql_error());
+}else {
+	 	$selectdb=mysql_select_db('info',$conn);
+	if (!$selectdb) {
+	 	echo 'Cannot use the Database'.mysql_error();
+	}
+	$sql="select * from grade";
+	$result=mysql_query($sql);
+	$rows=mysql_fetch_array($result);
+	$rows2=mysql_fetch_array($result,MYSQL_ASSOC);
+	print_r($result);
+	echo '<br/>';
+	print_r($rows);
+	echo '<br/>';
+	print_r($rows2);
+	echo '<br/>';
+	print_r($rows['name']);
+
 }
-flock($fp,LOCK_EX);//锁定
-    $string="<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n";
-    fwrite($fp,$string,strlen($string));
-    $string="<vip>\r\n";
-    fwrite($fp,$string,strlen($string));
-    $string="\t<id>5</id>\r\n";
-    fwrite($fp,$string,strlen($string));
-    $string="\t<username>face</username>\r\n";
-    fwrite($fp,$string,strlen($string));
-    $string="\t<sex>男</sex>\r\n";
-    fwrite($fp,$string,strlen($string));
-    $string="\t<face>images/face/m01.gif</face>\r\n";
-    fwrite($fp,$string,strlen($string));
-    $string="\t<email>cclovesky@gmail.com</email>\r\n";
-    fwrite($fp,$string,strlen($string));
-    $string="\t<url>xlzx.online</url>\r\n";
-    fwrite($fp,$string,strlen($string));
-    $string="</vip>\r\n";
-    fwrite($fp,$string,strlen($string));
-flock($fp,LOCK_UN);//解锁
-fclose($fp);
