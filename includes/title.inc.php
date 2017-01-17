fe0a15ada3f585d392bb9d75867fe7ff30dc9748
webfans <cc_lovesky@163.com>
2017-01-17 22:06:50 +0800

    加入动态网页标题

--- includes/title.inc.php
+++ includes/title.inc.php
@@ -7,7 +7,9 @@ if (!defined('IN_TG')){
 if (!defined('SCRIPT')){
     exit('script Error');
 }
+global $sys;
 ?>
+<title><?php echo $sys['webname'].'-'.SCRIPT;?></title>
 <link rel="stylesheet" type="text/css" href="styles/1/basic.css"/>
 <link rel="stylesheet" type="text/css" href="styles/1/<?php echo SCRIPT?>.css"/>
 <link rel="shortcut icon" href="images/heart2.ico">
