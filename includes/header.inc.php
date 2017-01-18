47348dc0010ce29ad492d3aeb999858f87d826c8
webfans <cc_lovesky@163.com>
2017-01-18 16:48:42 +0800

    newf ile

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -26,7 +26,7 @@ if (!defined('IN_TG')){
         ?>
 
         <li><a href="blog.php">博友</a></li>
-        <li><a href="photo.php">相册</a></li>
+        <li><a href="album.php">相册</a></li>
         <li>风格</li>
         <?php
         //if (isset($_COOKIE['username'])&& isset($_SESSION['admin'])){
@@ -34,7 +34,7 @@ if (!defined('IN_TG')){
         //}
         //上面这种写法有bug 如果前一个管理员登录，退出后没有清空相应的session,那么普通会员登录，都会神奇的变成管理员
         //为了代码健壮还应该判断sesion和cookie是否相等
-            if (isset($_COOKIE['username'])&&isset($_SESSION['admin'])) {
+            if (isset($_COOKIE['username']) && isset($_SESSION['admin'])) {
                 if ($_COOKIE['username']==$_SESSION['admin']){
                     echo '<li><a href="manage.php" class="admin">管理</a> </li>';
                 }
