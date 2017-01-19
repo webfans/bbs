df5467a11644975bbd7d95e0eca3e0f1b4bfc96a
webfans <cc_lovesky@163.com>
2017-01-19 17:53:03 +0800

    将图片信息入库

--- album_add_img.php
+++ album_add_img.php
@@ -13,13 +13,46 @@ require dirname(__FILE__).'/includes/common.inc.php';
 if (!isset($_COOKIE['username'])){
     alert_back('请登录后使用上传');
 }
+//保存图片信息入库
+if ($_GET['action']=='addimg'){
+    block_fake_cookie();
+    include ROOT_PATH.'includes/check.func.php';
+    //授收图片信息
+    $img=array();
+    $img['name']=check_length($_POST['imgname'],'图片名',2,20);
+    $img['url']=not_null($_POST['imgurl'],'图片地址');
+    $img['content']=$_POST['imgcontent'];
+    $img['sid']=$_POST['sid'];
+    $img=mysql_string($img);
+    //写入数据库
+    query("insert into bbs.bbs_photo(
+                                        photo_name, 
+                                        photo_url, 
+                                        photo_content, 
+                                        photo_date, 
+                                        photo_owner, 
+                                        photo_sid
+                                        )
+                                  VALUES(
+                                        '{$img['name']}',
+                                        '{$img['url']}',
+                                        '{$img['content']}',
+                                         NOW(),
+                                         '{$_COOKIE['username']}',
+                                        '{$img['sid']}'       
+                                        ) 
+              ");
+    after_query('图片添加成功','图片添加失败','album_show.php?id='.$img['sid']);
 
+}
+//取得相册数据
 if ($_GET['id']){
     if (!!$rows=fetch_array("select album_id,album_dir from bbs.bbs_album WHERE album_id='{$_GET['id']}'")){
         $get_album=array();
         $get_album['id']=$rows['album_id'];
         $get_album['dir']=$rows['album_dir'];
         $get_album=html_spec($get_album);
+
     }else{
         alert_back('不存在此相册');
     }
@@ -46,15 +79,16 @@ require ROOT_PATH.'includes/header.inc.php';
 
 <div id="album">
     <h2>上传照片</h2>
-    <form method="post" action="?action=adddir" name="addalbumdir">
+    <form method="post" action="?action=addimg" name="addimg">
+        <input type="hidden"  name="sid" value="<?php echo $get_album['id']?>"><!--照片所从属的相册-->
     <dl>
-        <dd>图片名称：<input type="text" name="albumname" class="text"> </dd>
-        <dd>图片地址：<input type="text" id='albumurl' name="albumurl" class="text" readonly>
+        <dd>图片名称：<input type="text" name="imgname" class="text"> </dd>
+        <dd>图片地址：<input type="text" id='imgurl' name="imgurl" class="text" readonly>
             <!--通过JS将title里面存放的上传目录的物理地址传给上传处理页upload.php-->
             <a href="javascript:;" title="<?php echo $get_album['dir']?>" id="upload">上传</a>
         </dd>
-        <dd>图片描述：<textarea name='albumcontent'></textarea></dd>
-        <dd><input type="submit" name="albumsubmit" class="submit" value="添加照片"></dd>
+        <dd>图片描述：<textarea name='imgcontent'></textarea></dd>
+        <dd><input type="submit" name="imgsubmit" class="submit" value="添加照片"></dd>
     </dl>
     </form>
 
