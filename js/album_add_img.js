e94f9a470f772d20b6112a7de1b98f3b24ce9f93
webfans <cc_lovesky@163.com>
2017-01-19 17:54:44 +0800

    加入JS验证

--- js/album_add_img.js
+++ js/album_add_img.js
@@ -3,8 +3,23 @@
  */
 window.onload=function () {
     var upload=document.getElementById('upload');
+    var fm=document.getElementsByTagName('form')[0];
     upload.onclick=function () {
         centerWindow('upload.php?dir='+this.title,'upload','100','400');
+    }
+    fm.onsubmit=function () {
+        //图片名验证
+        if (fm.imgname.value.length < 2 || fm.imgname.value.length > 20) {
+            alert('用户名不能小于2位或大于20位');
+            fm.imgname.focus();//将光标移到该字段
+            return false;
+        }
+        if (fm.imgurl.value=='') {
+            alert('图片url不能为空');
+            fm.imgurl.focus();//将光标移到该字段
+            return false;
+        }
+
     }
 }
 //保证窗口始终在中间
