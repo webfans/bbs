f97b3cca7bbfe75561b79c3563091a5431ad7627
webfans <cc_lovesky@163.com>
2017-01-20 15:53:43 +0800

    实现列出照片

--- album_show.php
+++ album_show.php
@@ -14,10 +14,11 @@ if (!isset($_COOKIE['username'])){
     alert_back('请登录后使用上传');
 }
 //取得相册数据
-if ($_GET['id']){
-    if (!!$rows=fetch_array("select album_id from bbs.bbs_album WHERE album_id='{$_GET['id']}'")){
+if ($_GET['dirid']){
+    if (!!$rows=fetch_array("select album_id,album_name from bbs.bbs_album WHERE album_id='{$_GET['dirid']}'")){
         $get_album=array();
         $get_album['id']=$rows['album_id'];
+        $get_album['name']=$rows['album_name'];
         $get_album=html_spec($get_album);
     }else{
         alert_back('不存在此相册');
@@ -26,8 +27,19 @@ if ($_GET['id']){
     alert_back('非法操作');
 }
 //取得照片数据
-$filename='album/1484808483/touxiang.jpg';
+//$filename='album/1484808483/touxiang.jpg';
 $zoom=0.3;
+global $_pagesize,$_pagenum,$sys;
+global $_id;
+$_id='dirid='.$get_album['id'].'&';
+$conut_sql="select photo_id from bbs.bbs_photo WHERE photo_sid='{$_GET['dirid']}'";
+paging_fault_tolerant($conut_sql,4);
+$photo_result=query("select photo_id,photo_name,photo_url,photo_owner
+                          from bbs.bbs_photo
+                          WHERE photo_sid='{$_GET['dirid']}'
+                          ORDER BY photo_date DESC 
+                          limit $_pagenum,$_pagesize
+                     ");
 
 
 ?>
@@ -46,9 +58,40 @@ require ROOT_PATH.'includes/header.inc.php';
 ?>
 
 <div id="album">
-    <h2>照片展示</h2>
-    <img src="thumb.php?filename=<?php echo $filename;?>&zoom=<?php echo $zoom;?>">
+    <h2>照片展示-<?php echo $get_album['name']?></h2>
+    <?php
+    $photo=array();
+    while(!!$rows=fetch_array_list($photo_result)){
+        $photo['id']=$rows['photo_id'];
+        $photo['name']=$rows['photo_name'];
+        $photo['url']=$rows['photo_url'];
+        $photo['owner']=$rows['photo_owner'];
+        $photo=html_spec($photo);
+        $filename=$photo['url'];
+        ?>
+    <dl>
+        <dt>
+            <a href="photo_detail.php?imgid=<?php echo $photo['id']?>">
+                <img src="thumb.php?filename=<?php echo $photo['url'];?>&zoom=<?php echo $zoom;?>">
+            </a>
+        </dt>
+        <dd>
+            <a href="photo_detail.php?imgid=<?php echo $photo['id']?>">
+                <?php echo $photo['name']?>
+            </a>
+        </dd>
+        <dd>阅(<strong>0</strong>)评(<strong>0</strong>)上传者:<?php echo $photo['owner']?></dd>
+    </dl>
+    <?php }?>
     <p><a href="album_add_img.php?id=<?php echo $get_album['id']?>">上传图片</a></p>
+
+    <?php
+    //销毁数据集
+    mysql_free_result($result);
+    //调用分页函数
+    paging(1);
+    paging(2);
+    ?>
 </div>
 
 
